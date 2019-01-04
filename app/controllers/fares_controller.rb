5ce8f7a9b5b2fcbde5589d9d769f684351acd040
EC2 Default User <ec2-user@ip-172-31-40-100.ap-southeast-1.compute.internal>
2019-01-04 13:13:16 +0000

    weather event api

--- app/controllers/fares_controller.rb
+++ app/controllers/fares_controller.rb
@@ -1,5 +1,86 @@
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
+
 class FaresController < ApplicationController
   before_action :set_fare, only: [:show, :edit, :update, :destroy]
+  
+  def get_event(keyword)
+  
+    params = URI.encode_www_form({keyword: keyword})
+    uri = URI.parse("https://eventon.jp/api/events.json?#{params}")
+    @query = uri.query
+
+    https = Net::HTTP.new(uri.host,uri.port)
+    https.use_ssl = true
+    https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    response = https.get(uri.request_uri)
+    
+    begin
+      case response
+      # 成功した場合
+      when Net::HTTPSuccess
+        @result = JSON.parse(response.body)
+        @title = @result["events"][0]["title"]
+        @place = @result["events"][0]["place"]
+        @date = @result["events"][0]["started_at"]
+      # 別のURLに飛ばされた場合
+      when Net::HTTPRedirection
+        @message = "redirection"
+      # その他エラー
+      else
+        @message = "error"
+      end
+    # エラー時処理
+    rescue IOError => e
+      @message = e.message
+    rescue TimeoutError => e
+      @message = e.message
+    rescue JSON::ParserError => e
+      @message = e.message
+    rescue => e
+      @message = e.message
+    end
+    
+  end
+  
+  # def get_weather(keyword)
+  
+  #   params = URI.encode_www_form({q: "osaka"})
+  #   uri = URI.parse("https://api.openweathermap.org/data/2.5/forecast?#{params}&units=metric&APPID=5307733b873da60f436160c4203d9fd7")
+  #   @query = uri.query
+
+  #   https = Net::HTTP.new(uri.host,uri.port)
+  #   https.use_ssl = true
+  #   https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  #   response = https.get(uri.request_uri)
+    
+  #   begin
+  #     case response
+  #     # 成功した場合
+  #     when Net::HTTPSuccess
+  #       @result = JSON.parse(response.body)
+  #       @weather = @result["list"][0]["weather"][0]["main"]
+  #     # 別のURLに飛ばされた場合
+  #     when Net::HTTPRedirection
+  #       @wether = "redirection"
+  #     # その他エラー
+  #     else
+  #       @weather = "error"
+  #     end
+  #   # エラー時処理
+  #   rescue IOError => e
+  #     @message = e.message
+  #   rescue TimeoutError => e
+  #     @message = e.message
+  #   rescue JSON::ParserError => e
+  #     @message = e.message
+  #   rescue => e
+  #     @message = e.message
+  #   end
+    
+  # end
 
   # GET /fares
   # GET /fares.json
@@ -10,6 +91,23 @@ class FaresController < ApplicationController
   # GET /fares/1
   # GET /fares/1.json
   def show
+    
+    get_event(@fare.ent)
+    @title_ent = @title
+    @date_ent  = @date
+    @place_ent = @place
+    
+    #get_weather(@fare.ent)
+    #@weather_ent = @weather
+    
+    get_event(@fare.ext)
+    @title_ext = @title
+    @date_ext  = @date
+    @place_ext = @place
+    
+    #get_weather(@fare.ext)
+    #@weather_ext = @weather
+
   end
 
   # GET /fares/new
@@ -72,3 +170,4 @@ class FaresController < ApplicationController
       params.require(:fare).permit(:ent, :ext, :price)
     end
 end
+
